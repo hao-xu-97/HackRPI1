cfefd16b4d24a2c79dbfc27674ba7c6ba14f0908
tomatosoldier <haoxunico@gmail.com>
2015-08-27 10:59:45 -0400

    no message

--- Pong/Assets/Scripts/AddForce.cs
+++ Pong/Assets/Scripts/AddForce.cs
@@ -15,6 +15,16 @@ public class AddForce : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(rb.velocity.magnitude < 300)
+			rb.AddForce (Random.Range (-100, 100), Random.Range (-100, 100), 0);
+		if (transform.position.Equals (new Vector3 (0, 0, 0))) {
+			rb.velocity = new Vector3(0,0,0);
+			rb.AddForce (horiSpeed, Random.Range(300,1000) , 0);
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "paddle_0" || col.gameObject.name == "paddle_1")
+			rb.AddForce (Random.Range (-100, 100), Random.Range (-100, 100), 0);
 	}
 }
---
+++ Pong/Assets/Scripts/AddForceRegular.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceRegular : MonoBehaviour {
+	public int horiSpeed;
+	private Rigidbody rb;
+	
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce (horiSpeed, Random.Range(400,600) , 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.Equals (new Vector3 (0, 0, 0))) {
+			rb.velocity = new Vector3(0,0,0);
+			rb.AddForce (horiSpeed, Random.Range(400,600) , 0);
+		}
+	
+	}
+}
